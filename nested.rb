5929d6cfe730139ac2cb45f7489708b85ea62b36
Dan Romans <danieljromans@gmail.com>
2019-08-03 07:18:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,7 +135,9 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+#programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+
+programmer_hash[:dennis_ritchie][:languages] ? programmer_hash[:dennis_ritchie][:languages] += "Assembly" : nil
 
 return programmer_hash
 
